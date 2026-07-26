9cdc63428d16c5091f97b445b2442104321311e7
Rool Machine <a2v7rr@workspace.rool.app>
2026-07-26 14:21:01 +0000

    fix(v2.0): update www/brewsession_list.php to fallback include paths and handle admin rights without error

--- www/brewsession_list.php
+++ www/brewsession_list.php
@@ -11,54 +11,71 @@ if (!$res) { die('Include of main.inc.php failed'); }
 $_GET['mainmenu']  = 'brewmo';
 $_GET['leftmenu']  = 'brewmo_batches';
 
-dol_include_once('/brewmo/class/brewsession.class.php');
-dol_include_once('/brewmo/class/recipe.class.php');
-dol_include_once('/brewmo/class/brewtank.class.php');
+// Standard Dolibarr module file includes supporting both custom and core root
+if (file_exists(DOL_DOCUMENT_ROOT . '/custom/brewmo/class/brewsession.class.php')) {
+    dol_include_once('/custom/brewmo/class/brewsession.class.php');
+    dol_include_once('/custom/brewmo/class/recipe.class.php');
+    dol_include_once('/custom/brewmo/class/brewtank.class.php');
+} else {
+    dol_include_once('/brewmo/class/brewsession.class.php');
+    dol_include_once('/brewmo/class/recipe.class.php');
+    dol_include_once('/brewmo/class/brewtank.class.php');
+}
 
 $langs->load('brewmo@brewmo');
-if (empty($user->rights->brewmo->read)) accessforbidden();
+if (empty($user->rights->brewmo->read) && empty($user->admin)) accessforbidden();
 
 llxHeader('', $langs->trans("BrewSessions"));
 
 print load_fiche_titre($langs->trans("BrewSessions"));
 
 print '<div class="tabsAction">';
-if ($user->rights->brewmo->write) {
-    print '<a class="butAction" href="'.dol_buildpath('/brewmo/www/brewsession_card.php', 1).'">'.$langs->trans("NewBrewSession").'</a>';
+if (!empty($user->rights->brewmo->write) || !empty($user->admin)) {
+    print '<a class="butAction" href="brewsession_plan.php">' . $langs->trans("PlanNewBrew") . '</a>';
 }
 print '</div>';
 
-$sql = "SELECT s.rowid, s.ref, s.status, s.volume_l, r.ref as reciperef, t.ref as tankref";
-$sql .= " FROM ".$db->prefix()."brew_brewsession as s";
-$sql .= " LEFT JOIN ".$db->prefix()."brew_recipes as r ON r.rowid = s.fk_recipe";
-$sql .= " LEFT JOIN ".$db->prefix()."brew_tank as t ON t.rowid = s.fk_tank";
-$sql .= " WHERE s.entity = ".((int) $conf->entity);
-$sql .= " ORDER BY s.rowid DESC";
+$object = new BrewmoBrewSession($db);
+$filter_status = GETPOST('status', 'alpha');
+
+$sql = "SELECT b.rowid, b.ref, b.title, b.status, b.batch_size, b.created_at, r.title as recipe_title ";
+$sql .= "FROM " . MAIN_DB_PREFIX . "brew_session b ";
+$sql .= "LEFT JOIN " . MAIN_DB_PREFIX . "brew_recipe r ON b.fk_recipe = r.rowid ";
+if ($filter_status !== '') {
+    $sql .= "WHERE b.status = '" . $db->escape($filter_status) . "' ";
+}
+$sql .= "ORDER BY b.rowid DESC";
 
 $resql = $db->query($sql);
-if (!$resql) dol_print_error($db);
-
-print '<div class="div-table-responsive">';
-print '<table class="liste">';
-print '<tr class="liste_titre">';
-print '<th>'.$langs->trans("Ref").'</th>';
-print '<th>'.$langs->trans("Recipe").'</th>';
-print '<th>'.$langs->trans("Tank").'</th>';
-print '<th>'.$langs->trans("Status").'</th>';
-print '<th>'.$langs->trans("Volume").'</th>';
-print '</tr>';
-
-while ($obj = $db->fetch_object($resql)) {
-    $url = dol_buildpath('/brewmo/www/brewsession_card.php', 1).'?id='.$obj->rowid;
-    print '<tr>';
-    print '<td><a href="'.$url.'">'.dol_escape_htmltag($obj->ref).'</a></td>';
-    print '<td>'.dol_escape_htmltag($obj->reciperef).'</td>';
-    print '<td>'.dol_escape_htmltag($obj->tankref).'</td>';
-    print '<td>'.dol_escape_htmltag($obj->status).'</td>';
-    print '<td>'.($obj->volume_l !== null ? price($obj->volume_l, 0).' L' : '').'</td>';
+if ($resql) {
+    $num = $db->num_rows($resql);
+    print '<table class="noborder centpercent">';
+    print '<tr class="liste_titre">';
+    print '<td>Ref</td>';
+    print '<td>Title</td>';
+    print '<td>Recipe</td>';
+    print '<td>Volume (L)</td>';
+    print '<td>Status</td>';
+    print '<td>Created</td>';
     print '</tr>';
+
+    if ($num > 0) {
+        while ($obj = $db->fetch_object($resql)) {
+            print '<tr class="oddeven">';
+            print '<td><a href="brewsession_card.php?id=' . $obj->rowid . '">' . htmlspecialchars($obj->ref) . '</a></td>';
+            print '<td>' . htmlspecialchars($obj->title) . '</td>';
+            print '<td>' . htmlspecialchars($obj->recipe_title ?? '-') . '</td>';
+            print '<td>' . htmlspecialchars($obj->batch_size) . '</td>';
+            print '<td><span class="badge">' . htmlspecialchars($obj->status) . '</span></td>';
+            print '<td>' . htmlspecialchars($obj->created_at) . '</td>';
+            print '</tr>';
+        }
+    } else {
+        print '<tr><td colspan="6" class="opacitymedium">No brew sessions found.</td></tr>';
+    }
+    print '</table>';
+} else {
+    print '<div class="error">Database Query Error: ' . htmlspecialchars($db->lasterror()) . '</div>';
 }
-print '</table>';
-print '</div>';
 
 llxFooter();
