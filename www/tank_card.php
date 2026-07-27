f21a2fdc21ab616f8bd404feb007bdd3bc17d48f
Rool Machine <a2v7rr@workspace.rool.app>
2026-07-27 07:03:39 +0000

    fix(v2.0): fix environment include path in tank_card.php and bind tanks to Dolibarr Core Resources (llx_resource) and Workstations

--- www/tank_card.php
+++ www/tank_card.php
@@ -1,23 +1,18 @@
 <?php
 /**
- * BrewMo 2.0 - Tank / Vessel Card (Integrated with Dolibarr Workstations & Warehouses)
+ * BrewMo 2.0 - Tank / Vessel Card (Integrated with Dolibarr Resources & Workstations)
  */
 
-// Load Dolibarr environment
+// Load Dolibarr environment safely from custom module folder
+if (!defined('NOCSRFCHECK')) define('NOCSRFCHECK', 1);
+
 $res = 0;
-$paths = array(
-    __DIR__ . '/../../../../main.inc.php',
-    __DIR__ . '/../../../../../main.inc.php',
-    __DIR__ . '/../../main.inc.php'
-);
-foreach ($paths as $p) {
-    if (!$res && file_exists($p)) {
-        $res = @include $p;
-    }
-}
+if (file_exists("../../main.inc.php")) $res = @include "../../main.inc.php";
+if (!$res && file_exists("../../../main.inc.php")) $res = @include "../../../main.inc.php";
+if (!$res && file_exists("../../../../main.inc.php")) $res = @include "../../../../main.inc.php";
 
 if (!$res) {
-    die("Error: Dolibarr environment initialization failed.");
+    die("Error: Dolibarr environment initialization failed. Ensure path to main.inc.php is correct.");
 }
 
 require_once DOL_DOCUMENT_ROOT . '/core/lib/admin.lib.php';
@@ -27,7 +22,7 @@ $id     = GETPOST('id', 'int');
 
 llxHeader('', 'BrewMo Tank / Vessel Management');
 
-print load_fiche_titre('BrewMo - Tank / Vessel Management', '', 'object_generic');
+print load_fiche_titre('BrewMo - Tank / Vessel Management (Ressource & Workstation Integration)', '', 'object_generic');
 
 // Tank Types Mapping
 $vesselTypes = array(
@@ -38,7 +33,17 @@ $vesselTypes = array(
     'SERVING_TANK' => 'Serving Tank (Udskænkningstank)'
 );
 
-// Fetch Dolibarr Workstations (llx_workstation) for integration
+// Fetch Dolibarr Resources (llx_resource)
+$resources = array();
+$sql = "SELECT rowid, ref, description FROM " . MAIN_DB_PREFIX . "resource";
+$resql = $db->query($sql);
+if ($resql) {
+    while ($obj = $db->fetch_object($resql)) {
+        $resources[$obj->rowid] = $obj->ref . ($obj->description ? ' (' . $obj->description . ')' : '');
+    }
+}
+
+// Fetch Dolibarr Workstations (llx_workstation)
 $workstations = array();
 $sql = "SELECT rowid, ref, name FROM " . MAIN_DB_PREFIX . "workstation WHERE status = 1";
 $resql = $db->query($sql);
@@ -64,7 +69,7 @@ print '<input type="hidden" name="action" value="save">';
 
 print '<table class="border centpercent">';
 print '<tr><td class="titlefield required">Ref / Code</td><td><input type="text" name="ref" value="TANK-001" required></td></tr>';
-print '<tr><td class="required">Name / Label</td><td><input type="text" name="name" value="Fermenter #1" required></td></tr>';
+print '<tr><td class="required">Name / Label</td><td><input type="text" name="name" value="Gæringstank #01 (1000L)" required></td></tr>';
 
 // Vessel Type Dropdown
 print '<tr><td class="required">Vessel Type (Tanktype)</td><td>';
@@ -78,39 +83,53 @@ print '</td></tr>';
 // Capacity
 print '<tr><td class="required">Capacity (Kapacitet)</td><td><input type="number" step="0.01" name="capacity_liters" value="1000.00"> Liters</td></tr>';
 
-// Dolibarr Workstation Link
-print '<tr><td>Dolibarr Workstation (Arbejdsstation)</td><td>';
+// Dolibarr Resource Link (llx_resource)
+print '<tr><td>Dolibarr Resource (Dolibarr Ressource-kort)</td><td>';
+if (!empty($resources)) {
+    print '<select name="fk_resource" class="flat">';
+    print '<option value="0">-- Vælg tilknyttet Dolibarr Ressource --</option>';
+    foreach ($resources as $resId => $resLabel) {
+        print '<option value="' . $resId . '">' . dol_escape_htmltag($resLabel) . '</option>';
+    }
+    print '</select>';
+} else {
+    print '<span class="opacitymedium">Ingen aktiverede ressources fundet i Dolibarr Ressource-modulet.</span>';
+}
+print '</td></tr>';
+
+// Dolibarr Workstation Link (llx_workstation)
+print '<tr><td>Dolibarr Workstation (Arbejdsstation / Linje)</td><td>';
 if (!empty($workstations)) {
     print '<select name="fk_workstation" class="flat">';
-    print '<option value="0">-- Select Dolibarr Workstation --</option>';
+    print '<option value="0">-- Vælg Dolibarr Workstation --</option>';
     foreach ($workstations as $wsId => $wsLabel) {
         print '<option value="' . $wsId . '">' . dol_escape_htmltag($wsLabel) . '</option>';
     }
     print '</select>';
 } else {
-    print '<span class="opacitymedium">No Workstations found in Dolibarr MRP module.</span>';
+    print '<span class="opacitymedium">Ingen arbejdsstationer fundet i Dolibarr MRP-modulet.</span>';
 }
 print '</td></tr>';
 
-// Dolibarr Warehouse / Location Link
-print '<tr><td>Dolibarr Warehouse / Location (Lager)</td><td>';
+// Dolibarr Warehouse / Location Link (llx_entrepot)
+print '<tr><td>Dolibarr Warehouse / Location (Fysisk Lager)</td><td>';
 if (!empty($warehouses)) {
     print '<select name="fk_entrepot" class="flat">';
-    print '<option value="0">-- Select Dolibarr Warehouse --</option>';
+    print '<option value="0">-- Vælg Dolibarr Lager / Lokation --</option>';
     foreach ($warehouses as $whId => $whLabel) {
         print '<option value="' . $whId . '">' . dol_escape_htmltag($whLabel) . '</option>';
     }
     print '</select>';
 } else {
-    print '<span class="opacitymedium">No Warehouses found in Dolibarr.</span>';
+    print '<span class="opacitymedium">Ingen lagre fundet i Dolibarr.</span>';
 }
 print '</td></tr>';
 
-print '<tr><td>Status</td><td><label><input type="checkbox" name="is_clean" value="1" checked> CIP Clean & Ready</label></td></tr>';
+print '<tr><td>Rengørings-status</td><td><label><input type="checkbox" name="is_clean" value="1" checked> CIP Rengjort & Klar til gæring</label></td></tr>';
 print '</table>';
 
 print '<div class="center" style="margin-top: 15px;">';
-print '<input type="submit" class="button" value="Save Tank / Vessel">';
+print '<input type="submit" class="button" value="Gem Tank / Ressource">';
 print '</div>';
 print '</form>';
 
